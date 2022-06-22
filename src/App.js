b6a11144c5b657b9cdc1a437c706ed69cb6d02e7
Arkadiusz Zygan <a.zygan@gmail.com>
2022-06-22 15:52:22 +0200

    Add weather info

--- src/App.js
+++ src/App.js
@@ -1,28 +1,101 @@
-import { useEffect, useState } from "react";
-import { data, apiUrl } from "./helpers/fetch";
+import { useEffect, useState, useRef } from "react";
+import { data } from "./helpers/fetch.js";
 
 function App() {
-  const [query, setQuery] = useState("Kielce");
-  const url = apiUrl(query);
+  const [weather, setWeather] = useState({});
+  const [homeTown, setHomeTown] = useState("");
+  const [error, setError] = useState(false);
+  let homeTownRef = useRef("");
+  let cityName = useRef();
 
-  const search = (event) => {
-    if (event.key === "Enter") data(url);
+  const getWeather = async () => {
+    if (cityName.current.value !== "") {
+      const response = await data(cityName.current.value);
+      console.log(response.cod);
+      setWeather(response);
+      if (response.cod === 200) {
+        setError(false);
+      } else {
+        setError(true);
+      }
+    } else if (
+      homeTown !== homeTownRef.current.value &&
+      homeTownRef.current.value !== ""
+    ) {
+      const response = await data(homeTownRef.current.value);
+      console.log(response);
+      if (response.cod === 200) {
+        setError(false);
+      } else {
+        setError(true);
+      }
+
+      setWeather(response);
+    } else {
+      const response = await data(homeTown);
+      console.log(response);
+      setWeather(response);
+      if (response.cod === 200) {
+        setError(false);
+      } else {
+        setError(true);
+      }
+    }
+    homeTownRef.current.value = "";
+    cityName.current.value = "";
+  };
+
+  console.log(weather);
+
+  const addHomeTown = () => {
+    localStorage.setItem("homeTown", homeTownRef.current.value);
+    getWeather();
   };
 
   useEffect(() => {
-    data(url);
-  }, []);
+    if (localStorage.getItem("homeTown")) {
+      setHomeTown(localStorage.getItem("homeTown"));
+    }
+    getWeather();
+  }, [cityName, homeTown]);
+  console.log(weather.name);
 
   return (
-    <div className="app">
-      <h1>Local weather</h1>
-      <input
-        type="text"
-        onChange={(event) => setQuery(event.target.value)}
-        value={query}
-        onKeyPress={search}
-      />
-    </div>
+    <>
+      <div className="app">
+        <main>
+          <h1>
+            {weather.name} <sup>{weather.sys?.country}</sup>
+          </h1>
+          <h3>{weather?.main?.temp} °C</h3>
+          <p>max {weather.main?.temp_max}</p>
+          <p>feels {weather.main?.feels_like}</p>
+          <p>min {weather.main?.temp_min}</p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`}
+            height={100}
+          />
+          <p>Chmury{weather?.clouds?.all}%</p>
+          <p>Wilgotność{weather?.main?.humidity}%</p>
+          <p>Wiatr{weather?.wind?.speed}km/h</p>
+          <p>Ciśnienie{weather?.main?.pressure} hPa</p>
+          <p>
+            Wschód: {new Date(weather.sys?.sunrise * 1000).toLocaleTimeString()}
+          </p>
+          <p>
+            Zachód: {new Date(weather.sys?.sunset * 1000).toLocaleTimeString()}
+          </p>
+          <input type="text" ref={cityName} />
+          {error && <h3 style={{ color: "red" }}>Nie znaleziono miasta...</h3>}
+          <button onClick={getWeather}>Sprawdź</button>l
+        </main>
+        <footer>
+          <p>Twoje miasto</p>
+          <input type="text" ref={homeTownRef} />
+          <button onClick={addHomeTown}>Add</button>
+        </footer>
+      </div>
+    </>
   );
 }
 
--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,14 +1,13 @@
 import { api } from "../api/api";
 
-export const data = (url) => {
-  return fetch(url)
+export const data = async (query) => {
+  let weather;
+  let url = `${api.base}${query}&APPID=${process.env.REACT_APP_KEY}${api.metric}`;
+  return await fetch(url)
     .then((res) => res.json())
     .then((result) => {
-      console.log(result);
-    });
-};
+      weather = result;
 
-export const apiUrl = (query) => {
-  let url = `${api.base}${query}&APPID=${process.env.REACT_APP_KEY}${api.metric}`;
-  return url;
+      return weather;
+    });
 };
